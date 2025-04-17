e4a65702ab2384d31a18cfd7d17910f33b830a69
pablox85 <pdpcorrales@gmail.com>
2025-04-17 17:14:06 +0000

    delimitador

---
+++ _strtok.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - Entre point
+ *
+ * Return: 0 if succes
+ */
+
+int main (void){
+
+	int recorrer = 0;
+	char tab = 9; // tabulacion
+	char esp = 32; // espacio
+
+
+}
