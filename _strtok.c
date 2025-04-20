e069be66a3878ef096b297c7e8af962e5a49e42a
pablox85 <pdpcorrales@gmail.com>
2025-04-20 14:41:25 -0300

    debug

--- _strtok.c
+++ _strtok.c
@@ -12,25 +12,30 @@ int main (void)
 {
 
 	int indice = 0, flagInWord = 0, contadorDeRecorrer = 0;
-	char *recorrer = "( ) hola mundo/putos	<-tabulaso "\" / ";
+	char *recorrer = "( ) hola mundo/putos	<-tabulaso";
 	char tab = 9; // tabulacion
 	char esp = 32; // espacio
 
-	while (recorrer[indice] != '\0')	{
-		if (recorrer[indice] != 9 || recorrer[indice] != 32) {
+	printf("La string es: %s\n", recorrer); //debug
+
+	while (recorrer[indice] != '\0')
+	{
+		if (recorrer[indice] != 9 && recorrer[indice] != 32)
+		{
 			if (flagInWord == 0)
 			{
 				contadorDeRecorrer++;
 				flagInWord = 1;
 			}
-			if (recorrer[indice] == 9 || recorrer[indice] == 32) {
-				flagInWord = 0;
-				indice++;
-			}
-
 		}
-		printf ("pablabras: %d"\n, contadorDeRecorrer); //form VS
-		
-	}
+		else
+		{
+			flagInWord = 0;
+		}
+		indice++;
+	}		
+		printf ("pablabras: %d\n", contadorDeRecorrer);
+	
+	return (0);
 
 }
